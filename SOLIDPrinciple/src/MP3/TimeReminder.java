af6a54d23a714d27c2d211d27b2b92559f968d19
JoonsangLee <joonsang0328@gmail.com>
2026-03-24 18:17:50 +0900

    ELSE 문이랑 주석 추가해서 COMMIT

--- SOLIDPrinciple/src/MP3/TimeReminder.java
+++ SOLIDPrinciple/src/MP3/TimeReminder.java
@@ -9,8 +9,16 @@ public class TimeReminder {
 		m = new MP3();
 		int hour = cal.get(Calendar.HOUR_OF_DAY);
 		
+		
+		
 		if(hour >= 22) {
 			m.playSong();
+			
+			
+		}
+		//pull 이후 else 문 추가
+		else {
+			System.out.println("10시안지남!");
 		}
 	}
 }
